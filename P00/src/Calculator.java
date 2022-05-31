4c4940f08e06458b99a92ab6c4d74dbf23118082
eddisonchiek <21034014@myrp.edu.sg>
2022-05-31 15:22:17 +0800

    First push to P00 repository to GitHub

--- P00/src/Calculator.java
+++ P00/src/Calculator.java
@@ -1,10 +1,36 @@
 
 public class Calculator {
 	public int add(int a, int b) {
-		return a + b;
+		int total = a + b;
+		if((a > 9999 && a < 0) && ( b > 9999 && b < 0)) {
+			total = 0;
+		}
+		return total;
+		
 	}
 	public int subtract(int a,int b) {
-		return a - b;
+		int total = a - b;
+		if((a > 9999 && a < 0) && ( b > 9999 && b < 0)) {
+			total = 0;
+		}
+		return total;
+	}
+	public int multiply(int a,int b) {
+		int total = a * b;
+	     if((a > 9999 && a < 0) && ( b > 9999 && b < 0)) {
+		total = 0;
+	}
+		return a * b;
+	}
+	public int divide (int a, int b) {
+		int total =  a/b;
+		if(b ==0) {
+			throw new IllegalArgumentException();}
+		if((a > 9999 && a < 0) && ( b > 9999 && b < 0)) {
+			total = 0;
+		}
+			return total;
+		}
 	}
 
-}
+
--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -35,7 +35,7 @@ public class CalculatorTest {
 		int actual = cal.add(a, b);
 		
 		int expected = 9999;
-		assertEquals (expected, actual);
+		assertEquals ("",expected, actual);
 	}
 	@Test
 	public final void testSubtract() {
@@ -47,6 +47,108 @@ public class CalculatorTest {
 		
 		int expected = 5555;
 		assertEquals(actual, expected);
+	}
+	@Test
+	public final void testMultiply() {
+		int a = 1234;
+		int b = 5;
+		Calculator cal = new Calculator();
+		int actual= cal.multiply(a, b);
+		int expected = 6170;
+		assertEquals(actual, expected);
+	}
+	@Test
+	public final void testDivide() {
+		int a = 1000;
+		int b = 5;
+		Calculator cal = new Calculator();
+		int actual = cal.divide(a, b);
+		int expected = 200;
+		assertEquals(actual,expected);
+	}
+	@Test
+	public final void testAddError() {
+		int a = -10;
+		int b = 10000;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		String expected = "Error!";
+		assertNotEquals (expected, actual);
+		
+	}
+	@Test
+	public final void testSubtractError() {
+		int a = -10;
+		int b = 10000;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		String expected = "Error!";
+		assertNotEquals (expected, actual);
+		
+	}
+	@Test
+	public final void testMultiplyError() {
+		int a = -10;
+		int b = 10000;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		String expected = "Error!";
+		assertNotEquals (expected, actual);
+		
+	}
+	@Test
+	public final void testDivisionError() {
+		int a = -10;
+		int b = 10000;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		String expected = "Error!";
+		assertNotEquals (expected, actual);
+		
+	}
+	@Test
+	public final void testAddboundary() {
+		int a = 0;
+		int b = 0;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 0;
+		assertEquals (expected, actual);
+	}
+	@Test
+	public final void testSubtractboundary() {
+		int a = 0;
+		int b = 0;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 0;
+		assertEquals (expected, actual);
+	}
+	@Test
+	public final void testMultiplyboundary() {
+		int a = 0;
+		int b = 0;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 0;
+		assertEquals (expected, actual);
+	}
+	@Test
+	public final void testDivisionboundary() {
+		int a = 0;
+		int b = 0;
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 0;
+		assertEquals (expected, actual);
 	}
 
 }
