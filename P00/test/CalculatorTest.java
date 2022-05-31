12716c3da3e33609659d6e30cbeff26f9c8ca068
eddisonchiek <21034014@myrp.edu.sg>
2022-05-31 11:30:27 +0800

    first push to P00 repository in GitHub

--- P00/test/CalculatorTest.java
+++ P00/test/CalculatorTest.java
@@ -6,7 +6,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class CalculatorTest {
+public class Calculatortest {
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
